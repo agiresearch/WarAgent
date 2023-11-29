c19d1e87f8477b4818082531ad3da6cb4ccf4055
Wenyue Hua <28013619+Wenyueh@users.noreply.github.com>
2023-11-28 20:26:48 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,26 @@
+"""
+Copyright 2023 Wenyue Hua
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+"""
+
+__author__ = "Wenyue Hua"
+__copyright__ = "Copyright 2023, WarAgent"
+__date__ = "2023/11/28"
+__license__ = "Apache 2.0"
+__version__ = "0.0.1"
+
 from history_setting.action_definition import *
 from procoder.prompt import *
 from history_setting.action_definition import action_property_definition
